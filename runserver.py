4330a1857baac5a329ef6715bb300ac7bb718549
Jonathan White <hemepositive@gmail.com>
2014-10-23 07:27:24 +0000

    login test

--- runserver.py
+++ runserver.py
@@ -2,4 +2,7 @@ from app import app
 
 # analog to django runserver function
 
-app.run()
+#app.run()
+
+# nitrous.io
+app.run(host='0.0.0.0', port=3000)
--- tests.py
+++ tests.py
@@ -19,13 +19,63 @@ class AllTests(unittest.TestCase):
     def tearDown(self):
         db.drop_all()
         
-    def test_admin_user_creation(self):
+    def test_admin_setup(self):
         admin_user = Admin(
             username='test_username', 
             password= 'test_password'
         )
         db.session.add(admin_user)
         db.session.commit()
+        
+    # MODELS tests
+    
+    def test_admin_exists_in_database(self):
+        admin_user = Admin(
+            username='test_username', 
+            password= 'test_password'
+        )
+        db.session.add(admin_user)
+        db.session.commit()
+        # using Flask_SQLAlchemy method
+        admin_get = Admin.query.get(1)
+        assert admin_get.username == 'test_username'
+        
+    def test_poll_creation(self):
+        new_poll = Poll(
+            question='test_question?'
+        )
+        db.session.add(new_poll)
+        db.session.commit()
+        # using Flask_SQLAlchemy method
+        all_polls = Poll.query.all()
+        for poll in all_polls:
+            poll.question
+        assert poll.question == 'test_question?'
+    
+    # VIEWS tests
+    
+    def test_url_route_for_admin(self):
+        resp = self.app.get('/admin')
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn('Flask administration', resp.data)
+    
+    def login_helper(self, username, password):
+        ''' A helper function for testing admin '''
+        return self.app.post('/admin', 
+                            data=dict(
+                                username=username,
+                                password=password
+                            ),
+                            follow_redirects=True
+                            )
+    
+    def test_non_Admin_cannot_access_dashboard(self):
+        resp = self.login_helper('wrong', 'wrongagain')
+        self.assertIn("Invalid username or password.", resp.data)
+        
+    
+    
+        
         
         
     
